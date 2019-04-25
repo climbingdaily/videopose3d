24c3839b94d64740e6186bca97fbc8540d9bd625
daidai1230 <dyd23@qq.com>
2019-04-25 17:02:51 +0800

    Update wild_video.py

--- wild_video.py
+++ wild_video.py
@@ -153,10 +153,10 @@ prediction = camera_to_world(prediction, R=rot, t=tran)
 
 # We don't have the trajectory, but at least we can rebase the height
 print(np.min(prediction[:, :, 2]))
-print(prediction[:2])
+print('before\n', prediction[:2])
 prediction[:, :, 2] -= np.min(prediction[:, :, 2])
-print(prediction[:2])
-print(prediction[:2,:2,2])
+print('after\n', prediction[:2])
+print('test\n', rediction[:, :, 2])
 sys.exit()
 
 anim_output = {'Reconstruction': prediction}
